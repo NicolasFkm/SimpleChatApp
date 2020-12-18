ecddae1e8a1e86d13a21525b222feb75b234dfce
NicolasFKM <nicolas.fukuyama@hotmail.com>
2020-12-17 22:59:03 -0300

    Simple user message identification

--- public/js/socket-script.js
+++ public/js/socket-script.js
@@ -1,14 +1,26 @@
 $(function () {
-    var socket = io();
+    const socket = io();
+
+    const username = prompt("What's your name?");
     
     $('form.chat-box').submit(function (e) {
         e.preventDefault();
-        socket.emit('chat message', $('#message').val());
+        socket.emit('chat message', { message: $('#message').val(), username});
         $('#message').val('');
         return false;
     });
 
     socket.on('chat message', function (msg) {
-        $('.history').append($('<li>').text(msg));
+
+        const div = $('<div>').addClass("message-box");
+        const message = $('<p>').text(msg.message);
+
+        if(msg.username == username){
+            message.addClass("sent");
+        }
+        div.append(message);
+        $('#history').append(div);
+        var objDiv = document.getElementById("history");
+        objDiv.scrollTop = objDiv.scrollHeight;
     });
 });
